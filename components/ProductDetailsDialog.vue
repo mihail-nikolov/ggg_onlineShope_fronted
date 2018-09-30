9fcd7fd3fe78fb3923d5518dcb0f7c0173596c4f
Mihail Nikolov <mihail.nikolov@ict.eu>
2018-09-30 23:53:37 +0300

    add interchangeableParts

--- components/ProductDetailsDialog.vue
+++ components/ProductDetailsDialog.vue
@@ -13,96 +13,102 @@
 								</v-flex>
 								<v-flex xs12 sm12 md6 column>
 									<v-flex xs12 column>
-										<v-card-text>
-											<div v-if="curProductDetails.Description">
-												<h4>Описание</h4>
-												<div>{{curProductDetails.Description}}</div>
-												<br>
-											</div>
-											<div class="text-container">
-												<div v-if="curProductDetails.EuroCode" class="floating-product-detail">
-													<h4>Eurocode</h4>
-													<div>{{curProductDetails.EuroCode}}</div>
-													<br>
-												</div>
-												<div v-if="curProductDetails.MaterialNumber" class="floating-product-detail">
-													<h4>Material Number</h4>
-													<div>{{curProductDetails.MaterialNumber}}</div>
-													<br>
-												</div>
-												<div v-if="curProductDetails.IndustryCode" class="floating-product-detail">
-													<h4>Industry Code</h4>
-													<div>{{curProductDetails.IndustryCode}}</div>
-													<br>
-												</div>
-												<div v-if="curProductDetails.OesCode" class="floating-product-detail">
-													<h4>Oes Code</h4>
-													<div>{{curProductDetails.OesCode}}</div>
-													<br>
-												</div>
-												<div v-if="curProductDetails.LocalCode" class="floating-product-detail">
-													<h4>Local Code</h4>
-													<div>{{curProductDetails.LocalCode}}</div>
-													<br>
-												</div>
-											</div>
-											<div style="clear:both">
-												<h4>Тип</h4>
-												<div>{{curProductDetails.ProductType}}</div>
-											</div>
-											<br>
-											<div class="text-container" style="clear:both">
-												<div v-if="curProductDetails.Height" class="floating-product-detail">
-													<h4>Височина: </h4>
-													<div>{{ curProductDetails.Height }}</div>
-													<br>
-												</div>
-												<div v-if="curProductDetails.Width" class="floating-product-detail">
-													<h4>Дължина </h4>
-													<div>{{ curProductDetails.Width }}</div>
-													<br>
-												</div>
-												<div v-if="curProductDetails.FittingTimeHours" class="floating-product-detail">
-													<h4>Време за монтаж: </h4>
-													<div>{{ curProductDetails.FittingTimeHours }}</div>
-													<br>
-												</div>
-												<div v-if="curProductDetails.Tint" class="floating-product-detail">
-													<h4>Нюанс: </h4>
-													<div>{{ curProductDetails.Tint }}</div>
-													<br>
-												</div>
-												<div style="clear:both">
-													<div v-if="curProductDetails.ModelDate" class="floating-product-detail">
-														<h4>Година на модела: </h4>
-														<div>{{ curProductDetails.ModelDate }}</div>
-														<br>
-													</div>
-													<div v-if="curProductDetails.PartDate" class="floating-product-detail">
-														<h4>Година на частта: </h4>
-														<div>{{ curProductDetails.PartDate }}</div>
-														<br>
-													</div>
-												</div>
-											</div>
-											<!--<div v-if="currentProductAvailability.length">-->
-												<!--<h4>Наличност</h4>-->
-												<!--<div class="availability-container">-->
-													<!--<div class="group" v-for="group in currentProductAvailability">-->
-														<!--<h5>{{ group.Group }} - {{ round(group.Price) }}лв</h5>-->
-														<!--<div v-for="(quantity, store) in group.StoreQUantities" v-bind:class="{ empty: quantity === 0 }">-->
-															<!--{{ store }} - {{ quantity }}бр.-->
-														<!--</div>-->
-													<!--</div>-->
-												<!--</div>-->
-											<!--</div>-->
-											<div v-if="curProductDetails.Accessories.length" style="clear:both">
-												<h4>Аксесоари</h4>
-												<div class="availability-container">
-													<h5 class="pointer" v-for="accessory in curProductDetails.Accessories" @click="openAccessoryDialog(accessory)">{{ accessory.Description }}</h5>
-												</div>
-											</div>
-										</v-card-text>
+                    <v-card-text>
+                      <div v-if="curProductDetails.Description">
+                        <h4>Описание</h4>
+                        <div>{{curProductDetails.Description}}</div>
+                        <br>
+                      </div>
+                      <div class="text-container">
+                        <div v-if="curProductDetails.EuroCode" class="floating-product-detail">
+                          <h4>Eurocode</h4>
+                          <div>{{curProductDetails.EuroCode}}</div>
+                          <br>
+                        </div>
+                        <div v-if="curProductDetails.MaterialNumber" class="floating-product-detail">
+                          <h4>Material Number</h4>
+                          <div>{{curProductDetails.MaterialNumber}}</div>
+                          <br>
+                        </div>
+                        <div v-if="curProductDetails.IndustryCode" class="floating-product-detail">
+                          <h4>Industry Code</h4>
+                          <div>{{curProductDetails.IndustryCode}}</div>
+                          <br>
+                        </div>
+                        <div v-if="curProductDetails.OesCode" class="floating-product-detail">
+                          <h4>Oes Code</h4>
+                          <div>{{curProductDetails.OesCode}}</div>
+                          <br>
+                        </div>
+                        <div v-if="curProductDetails.LocalCode" class="floating-product-detail">
+                          <h4>Local Code</h4>
+                          <div>{{curProductDetails.LocalCode}}</div>
+                          <br>
+                        </div>
+                      </div>
+                      <div style="clear:both">
+                        <h4>Тип</h4>
+                        <div>{{curProductDetails.ProductType}}</div>
+                      </div>
+                      <br>
+                      <div class="text-container" style="clear:both">
+                        <div v-if="curProductDetails.Height" class="floating-product-detail">
+                          <h4>Височина: </h4>
+                          <div>{{ curProductDetails.Height }}</div>
+                          <br>
+                        </div>
+                        <div v-if="curProductDetails.Width" class="floating-product-detail">
+                          <h4>Дължина </h4>
+                          <div>{{ curProductDetails.Width }}</div>
+                          <br>
+                        </div>
+                        <div v-if="curProductDetails.FittingTimeHours" class="floating-product-detail">
+                          <h4>Време за монтаж: </h4>
+                          <div>{{ curProductDetails.FittingTimeHours }}</div>
+                          <br>
+                        </div>
+                        <div v-if="curProductDetails.Tint" class="floating-product-detail">
+                          <h4>Нюанс: </h4>
+                          <div>{{ curProductDetails.Tint }}</div>
+                          <br>
+                        </div>
+                        <div style="clear:both">
+                          <div v-if="curProductDetails.ModelDate" class="floating-product-detail">
+                            <h4>Година на модела: </h4>
+                            <div>{{ curProductDetails.ModelDate }}</div>
+                            <br>
+                          </div>
+                          <div v-if="curProductDetails.PartDate" class="floating-product-detail">
+                            <h4>Година на частта: </h4>
+                            <div>{{ curProductDetails.PartDate }}</div>
+                            <br>
+                          </div>
+                        </div>
+                      </div>
+                      <!--<div v-if="currentProductAvailability.length">-->
+                      <!--<h4>Наличност</h4>-->
+                      <!--<div class="availability-container">-->
+                      <!--<div class="group" v-for="group in currentProductAvailability">-->
+                      <!--<h5>{{ group.Group }} - {{ round(group.Price) }}лв</h5>-->
+                      <!--<div v-for="(quantity, store) in group.StoreQUantities" v-bind:class="{ empty: quantity === 0 }">-->
+                      <!--{{ store }} - {{ quantity }}бр.-->
+                      <!--</div>-->
+                      <!--</div>-->
+                      <!--</div>-->
+                      <!--</div>-->
+                      <div v-if="curProductDetails.Accessories.length" style="clear:both">
+                        <h4>Аксесоари</h4>
+                        <div class="availability-container">
+                          <h5 class="pointer" v-for="accessory in curProductDetails.Accessories" @click="openAccessoryDialog(accessory)">{{ accessory.Description }}</h5>
+                        </div>
+                      </div>
+                      <div v-if="curProductDetails.InterchangeableParts.length" style="clear:both">
+                        <h4>Взаимнозаменяеми части</h4>
+                        <div class="availability-container">
+                          <h5 class="pointer" v-for="part in curProductDetails.InterchangeableParts" @click="onOpenInterchangeableDialog(part)">{{ part.Description }}</h5>
+                        </div>
+                      </div>
+                    </v-card-text>
 									</v-flex>
 								</v-flex>
                                 <v-flex style="padding-bottom: 8px;">
@@ -145,7 +151,12 @@
 				this.$emit("onOpenAccessoryDialog", acc);
 				productsRepository.getFullProduct(acc)
 					.then(console.warn);
-			}
+      },
+      onOpenInterchangeableDialog(acc) {
+        this.$emit("onOpenInterchangeableDialog", interchangeablePart);
+        productsRepository.getFullProduct(interchangeablePart)
+          .then(console.warn);
+      }
 		},
 		watch: {
 			dialogDetailsOpen(value) {
