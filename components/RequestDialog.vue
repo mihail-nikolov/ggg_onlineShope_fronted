f0e22287fee88c7df5195dfe70331fd2aeee6802
mihail-nikolov <mihail.y.nikolov@abv.bg>
2018-10-19 13:48:45 +0300

    change field names and add others

--- components/RequestDialog.vue
+++ components/RequestDialog.vue
@@ -2,16 +2,20 @@
 	<v-form style="margin: 0; width: 100%; background-color: #fafafa; padding: 20px;">
 		<div v-if="showSuccessMessage === false">
 			<form-component @onKeyUp="onKeyUp" elemType="text" v-model="name" fieldName="Име" :hasError="validation.hasError('name')" :firstError="validation.firstError('name')"></form-component>
-			<form-component @onKeyUp="onKeyUp" elemType="email" v-model="email" fieldName="Имейл" :hasError="validation.hasError('email')" :firstError="validation.firstError('email')"></form-component>
+			<form-component @onKeyUp="onKeyUp" elemType="email" v-model="email" fieldName="Имеил" :hasError="validation.hasError('email')" :firstError="validation.firstError('email')"></form-component>
 			<form-component @onKeyUp="onKeyUp" elemType="text" v-model="phone" fieldName="Телефон" :hasError="validation.hasError('phone')" :firstError="validation.firstError('phone')"></form-component>
 
 			<form-component @onKeyUp="onKeyUp" elemType="text" v-model="make" fieldName="Марка" :hasError="validation.hasError('make')" :firstError="validation.firstError('make')"></form-component>
 			<form-component @onKeyUp="onKeyUp" elemType="text" v-model="model" fieldName="Модел" :hasError="validation.hasError('model')" :firstError="validation.firstError('model')"></form-component>
-			<form-component @onKeyUp="onKeyUp" elemType="text" v-model="body" fieldName="Тип" :hasError="validation.hasError('body')" :firstError="validation.firstError('body')"></form-component>
-			<form-component @onKeyUp="onKeyUp" elemType="text" v-model="bodywork" fieldName="Каросерия" :hasError="validation.hasError('bodywork')" :firstError="validation.firstError('bodywork')"></form-component>
-			<form-component @onKeyUp="onKeyUp" elemType="text" v-model="frameCode" fieldName="Номер на рама" :hasError="validation.hasError('frameCode')" :firstError="validation.firstError('frameCode')"></form-component>
+		  <form-component @onKeyUp="onKeyUp" elemType="text" v-model="modelYear" fieldName="Година" :hasError="validation.hasError('modelYear')" :firstError="validation.firstError('modelYear')"></form-component>
+			<form-component @onKeyUp="onKeyUp" elemType="text" v-model="body" fieldName="Тип каросерия и брой врати" :hasError="validation.hasError('body')" :firstError="validation.firstError('body')"></form-component>
+			<form-component @onKeyUp="onKeyUp" elemType="text" v-model="productType" fieldName="Тип Продукт" :hasError="validation.hasError('productType')" :firstError="validation.firstError('productType')"></form-component>
+			<form-component @onKeyUp="onKeyUp" elemType="text" v-model="vin" fieldName="Номер на рама" :hasError="validation.hasError('vin')" :firstError="validation.firstError('vin')"></form-component>
 
 			<form-component @onKeyUp="onKeyUp" elemType="text" v-model="description" fieldName="Описание" :hasError="validation.hasError('description')" :firstError="validation.firstError('description')"></form-component>
+			<input type="checkbox" value="true" v-model="enquireToRuse"><label for="enquireToRuse">Запитване Русе</label>
+			<input type="checkbox" value="true" v-model="enquireToSofia"><label for="enquireToSofia">Запитване София</label>
+			</br>
 			<v-layout row>
 				<v-spacer></v-spacer>
 				<v-btn primary @click="onSubmit">Изпрати Запитване</v-btn>
@@ -42,10 +46,13 @@
 				phone: "",
 				make: "",
 				model: "",
+				modelYear: "",
 				body: "",
-				bodywork: "",
-				frameCode: "",
+				productType: "",
+				vin: "",
 				description: "",
+				enquireToRuse: false,
+				enquireToSofia: false,
 				showSuccessMessage: false
 			};
 		},
@@ -58,15 +65,18 @@
 
 			onSubmit() {
 				const model = {
-					name: this.name,
-					email: this.email,
-					phone: this.phone,
-					make: this.make,
-					model: this.model,
-					body: this.body,
-					bodywork: this.bodywork,
-					frameCode: this.frameCode,
-					description: this.description
+					Name: this.name,
+					Email: this.email,
+					Phone: this.phone,
+					Make: this.make,
+					Model: this.model,
+					ModelYear: this.modelYear,
+					Body: this.body,
+					ProductType: this.productType,
+					VIN: this.vin,
+					Description: this.description,
+					EnquireToRuse: this.enquireToRuse,
+					EnquireToSofia: this.enquireToSofia
 				};
 
 				this.$validate()
